538033fb351dab97e37aeefc012164172a0cff71
Onpreeya Jantakote <119285414+Onpreeya-Jantakote@users.noreply.github.com>
2025-08-16 01:55:20 +0700

    add link figma in image

--- src/app/work/components/MyProjects.tsx
+++ src/app/work/components/MyProjects.tsx
@@ -5,8 +5,29 @@ import { Stack } from "@mui/system";
 
 const projects = [
   {
+    image: "/projects/CISEVENTHUB.png",
+    title: "CIS EVENT HUB",
+    link: "https://www.figma.com/design/J039NPfIJfZIF39eYRQ2gj/CISEVENTHUB?node-id=287-113&t=MXx37JQiv9gbAeeA-1",
+    details: [
+      {
+        label: "Overview",
+        text: "CIS Event Hub is a user-centered web platform that makes university event participation simple and engaging. With clear navigation, intuitive event discovery, and responsive design, students can find and join events easily, while staff can manage them efficiently. Features like AI-assisted creation, Google Calendar sync, and real-time notifications enhance usability and keep users engaged.",
+      },
+      {
+        label: "My role",
+        text: "I wanted the system to be easy to use and clear, without complexity. Users can see various reviews from other users to make it easier for them to make decisions about using this website.",
+      },
+      {
+        label: "User",
+        text: "Student of Computer and Information Science and Teacher",
+      },
+      { label: "Platform", text: "Web Application" },
+    ],
+  },
+  {
     image: "/projects/ProjectMobileApp.png",
     title: "Project Mobile App",
+    link: "https://www.figma.com/design/nxnNTpNOHJtdiLJEvZeXTJ/Mobile?node-id=0-1&t=HkSWydvUPwIq7Dar-1",
     details: [
       {
         label: "Overview",
@@ -23,6 +44,7 @@ const projects = [
   {
     image: "/projects/FITWAY.png",
     title: "FIT WAY Project",
+    link: "https://www.figma.com/design/Wl4oo0HJUuzOcfU1v67vSR/FITWAY?node-id=0-1&t=JvxYK5vyjcsqgcGi-1",
     details: [
       {
         label: "Overview",
@@ -34,22 +56,6 @@ const projects = [
       },
       { label: "User", text: "18 - 35 years" },
       { label: "Platform", text: "Website" },
-    ],
-  },
-  {
-    image: "/projects/CISEVENTHUB.png",
-    title: "CIS EVENT HUB",
-    details: [
-      {
-        label: "Overview",
-        text: "CIS Event Hub is a user-centered web platform that makes university event participation simple and engaging. With clear navigation, intuitive event discovery, and responsive design, students can find and join events easily, while staff can manage them efficiently. Features like AI-assisted creation, Google Calendar sync, and real-time notifications enhance usability and keep users engaged.",
-      },
-      {
-        label: "My role",
-        text: "I wanted the system to be easy to use and clear, without complexity. Users can see various reviews from other users to make it easier for them to make decisions about using this website.",
-      },
-      { label: "User", text: "Computer and Information Science and teacher" },
-      { label: "Platform", text: "Web Application" },
     ],
   },
 ];
@@ -71,7 +77,11 @@ export default function MyWork() {
 
       <Stack spacing={6}>
         {projects.map((project, index) => (
-          <ProjectCard key={project.title} {...project} reverse={index % 2 === 1} />
+          <ProjectCard
+            key={project.title}
+            {...project}
+            reverse={index % 2 === 1}
+          />
         ))}
       </Stack>
     </Box>
