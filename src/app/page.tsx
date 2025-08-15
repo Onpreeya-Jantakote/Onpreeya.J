28641907535294619800b498e2b120d814c9a160
Onpreeya Jantakote <119285414+Onpreeya-Jantakote@users.noreply.github.com>
2025-08-15 23:32:25 +0700

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,36 @@
 
 import { Container, Typography, Box } from "@mui/material";
 import Navbar from "@/app/components/Navbar";
+import ProfileHeader from "@/app/about-me/components/ProfileHeader";
+import SkillSection from "@/app/about-me/components/SkillSection";
+import MyWork from "./work/components/MyProjects";
+
+const skillsDesign = [
+  { name: "Figma", percent: 90 },
+  { name: "Illustrator", percent: 80 },
+  { name: "Photoshop", percent: 70 },
+];
+
+const skillsProgramming = [
+  { name: "React", percent: 85 },
+  { name: "HTML/CSS", percent: 90 },
+  { name: "JavaScript", percent: 80 },
+];
 
 export default function ProfilePage() {
   return (
     <>
       <Navbar />
       <Container>
+        <ProfileHeader />
+        <Box mt={6}>
+          <Typography variant="h5" sx={{ fontWeight: "bold", mb: 2, color: "#4C69EB", textAlign: "center" }}>
+            My Skills
+          </Typography>
+          <SkillSection title="Design Skills" skills={skillsDesign} />
+          <SkillSection title="Programming Skills" skills={skillsProgramming} />
+          <MyWork />
+        </Box>
       </Container>
     </>
   );
