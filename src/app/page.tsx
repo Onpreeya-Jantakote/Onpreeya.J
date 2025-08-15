94593dc95047a22b3bf147f01ca9f5158bccfb80
Onpreeya Jantakote <119285414+Onpreeya-Jantakote@users.noreply.github.com>
2025-08-16 01:52:33 +0700

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,59 @@ import Navbar from "@/app/components/Navbar";
 import ProfileHeader from "@/app/about-me/components/ProfileHeader";
 import SkillSection from "@/app/about-me/components/SkillSection";
 import MyWork from "./work/components/MyProjects";
+import FloatingDock from "./components/FloatingDock";
+import ExperienceTimeline from "./about-me/components/ExperienceTimeline";
+import ContactForm from "./components/Footer";
 
 const skillsDesign = [
   { name: "Figma", percent: 90 },
-  { name: "Illustrator", percent: 80 },
-  { name: "Photoshop", percent: 70 },
+  { name: "Illustrator", percent: 50 },
+  { name: "Photoshop", percent: 60 },
 ];
 
 const skillsProgramming = [
   { name: "React", percent: 85 },
   { name: "HTML/CSS", percent: 90 },
   { name: "JavaScript", percent: 80 },
+  { name: "NextJS", percent: 60 },
 ];
 
 export default function ProfilePage() {
   return (
     <>
       <Navbar />
+
       <Container>
-        <ProfileHeader />
+        {/* About Me */}
+        <Box id="about-me">
+          <ProfileHeader />
+        </Box>
+
+        {/* My Skills */}
         <Box mt={6}>
-          <Typography variant="h5" sx={{ fontWeight: "bold", mb: 2, color: "#4C69EB", textAlign: "center" }}>
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: "bold",
+              mb: 2,
+              color: "#4C69EB",
+              textAlign: "center",
+            }}
+          >
             My Skills
           </Typography>
           <SkillSection title="Design Skills" skills={skillsDesign} />
           <SkillSection title="Programming Skills" skills={skillsProgramming} />
+          <ExperienceTimeline />
+        </Box>
+
+        {/* My Projects */}
+        <Box id="work" mt={6}>
           <MyWork />
         </Box>
       </Container>
+      <ContactForm />
+      <FloatingDock />
     </>
   );
 }
